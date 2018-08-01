c4e78bb46a90779a7fa2c34bf5bd0dabc6a153fe
Aline Bossi Pereira da Silva <Aline@MacBook-Pro-de-Aline.local>
2018-08-01 08:13:22 -0300

    create person

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hello </h1>
+        <p> teste </p>
+        <Person name="maria" age="30"> My hobbie is blabla </Person>
+        <Person name="aline" age="28"/>
+        <Person name="teste" age="22"/>
       </div>
     );
   }
